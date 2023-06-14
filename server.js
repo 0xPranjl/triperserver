e463caa8ad0d7ed953cb38d103b1b546db22c78c
0xpranjl <pranjal.maurya402@gmail.com>
2023-06-14 07:39:13 +0530

    addd

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
-});
+}); 
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
   socket.on("join_room", (data) => {
