910fcf462305d220c49d532665041c7b9dd0166a
Pranjal Maurya <79030501+0xPranjl@users.noreply.github.com>
2023-06-21 12:06:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,23 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 }); 
-
+var add={};
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
   socket.on("join_room", (data) => {
+    if(add[data]){
+       socket.to(data).emit("receive_message", add[data]);
+    }
+    //get(data.room);
     socket.join(data);
   });
   socket.on("send_message", (data) => {
+    //eval(data.room+"i"+"="+"'"+data.message+"'");
+    //set(data.room,data.message);
+    add[data.room]=data.message;
     socket.to(data.room).emit("receive_message", data);
   });
 });
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+server.listen(80, () => {
+  
 });
